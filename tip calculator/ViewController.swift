d282eecb5618fe0d916da2aa4944294f20aa8ebd
zhijuncao <30332692+zhijuncao@users.noreply.github.com>
2017-07-28 17:37:42 +0800

    uploading my project to github

---
+++ tip calculator/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  tip calculator
+//
+//  Created by 李俊 on 2017/7/25.
+//  Copyright © 2017年 make school. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var billAmountField: UITextField!
+
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    
+    @IBOutlet weak var tipAmountField: UITextField!
+    
+    @IBOutlet weak var totalAmountField: UITextField!
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+            if let billAmount = Double(billAmountField.text!) {
+                var tipPercentage = 0.0
+                
+                switch tipSelector.selectedSegmentIndex {
+                case 0:
+                    tipPercentage = 0.15
+                case 1:
+                    tipPercentage = 0.18
+                case 2:
+                    tipPercentage = 0.20
+                default:
+                    break
+                }
+                
+                let roundedBillAmount = round(100 * billAmount) / 100
+                let tipAmount = roundedBillAmount * tipPercentage
+                let roundedTipAmount = round(100 * tipAmount) / 100
+                let totalAmount = roundedBillAmount + roundedTipAmount
+                
+                if billAmountField.isEditing {
+                    billAmountField.text = String(format: "%.2f", roundedBillAmount)
+                }
+                tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+                totalAmountField.text = String(format: "%.2f", totalAmount)
+            } else {
+                //show error
+                billAmountField.text = ""
+                tipAmountField.text = ""
+                totalAmountField.text = ""
+            }
+    }
+
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+
+}
+
